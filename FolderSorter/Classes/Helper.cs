5c6fd6a8b3e169f4c9d699771f3045552e9292c3
Eric Chen <ericchen1248@gmail.com>
2018-01-22 00:09:20 +0800

    Add some helper classes

---
+++ FolderSorter/Classes/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSorter.Classes
+{
+    internal static class Helper
+    {
+        public static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            }
+            catch (IOException)
+            {
+                // the file is unavailable because it is:
+                // still being written to
+                // or being processed by another thread
+                // or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
